d3cf66beb1a94ee34431558cfb7512e4819041ef
Lola <lolitasav884@gmail.com>
2022-07-16 15:27:59 +0300

    redaction

--- HW2/Task1.py
+++ HW2/Task1.py
@@ -7,24 +7,7 @@
 # - 0,56 -> 11
 
 
-# str_num = input('введите вещественное число:' )
-# def Check_num(str_num):
-#     if str_num.isdigit():
-#         print(str_num)
-#     else:
-#         return('не число')  #не удалось правлильно выйти из проверки
-# print(Check_num(str_num))
-
-#str_num = 0
 str_num = str(input('введите вещественное число:' ))
-# def check_num():
-#     while True:
-        
-#         if str_num.isdigit():
-#             return str_num           #пока не получается с проверкой буду еще думать
-        
-#         print('incorrect')
-# print(check_num(str_num))
 sum = 0
 for i in str_num:
         if i!=".":
